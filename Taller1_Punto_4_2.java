9d77ddca6a5403eb82151021e7fc4bc7820d1f67
JoseMartinez117 <124361846+JoseMartinez117@users.noreply.github.com>
2023-02-23 14:01:55 -0500

    Add files via upload

---
+++ Taller1_Punto_4_2.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package taller1_punto_4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jose Manuel
+ */
+public class Taller1_Punto_4_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        String [] vec1 = new String [5];
+        boolean valido=true;
+        String aux="";
+        int i=0;
+    // Uso del FOR
+    
+        do{
+        
+            do{
+                System.out.print("\n Ingrese la palabra n"+(i+1)+":");;
+                aux=teclado.next();   
+                if(aux.matches("[a-zA-Z]*")){
+                    vec1[i]=aux;
+                    valido=true;
+                }else{
+                   System.out.println("\n Ingrese de manera correcta la palabra\n");
+                }
+            }while(valido==false);     
+            i++;
+        }while(i<vec1.length);
+        
+        i=0;
+        //inicio del proceso para el crucigrama
+        do{
+        
+            
+            int [] almacen = new int [vec1[i].length()]; //se crea la variable almacen para identificar si la letra esta disponible
+            
+            System.out.println("\n Palabra base para comparar "+vec1[i]+" \n");
+            int h=0;
+            
+           do{
+                
+                if(i==h){
+                    h++;
+                }
+
+                if(h>=5){
+                    break;
+                }
+                int j=0;
+                do{
+
+                    valido=false;
+                   int k=0;
+                    do{
+ 
+                        if(vec1[h].charAt(k)==vec1[i].charAt(j)&&almacen[j]!=1){
+
+                            System.out.println("La letra "+vec1[h].charAt(k)+" de la palabra "+vec1[h]+" en caja en la letra "+vec1[i].charAt(j)+" de las palabras "+ vec1[i]);
+                            valido=true;
+                            break;
+                        }
+                      k++;
+                    }while(k<vec1[h].length());
+                    
+                    if(valido==true){
+                        almacen[j]=1;
+                        break;
+                    }
+                   j++;
+                }while(j<vec1[i].length());
+                
+                h++;
+            }while(h<vec1.length);
+            
+          i++;  
+        }while(i<vec1.length);
+        
+        
+        
+        
+        
+        
+        
+
+        
+    }
+}
